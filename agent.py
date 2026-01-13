199e46b87695c92a36f948bfdab409d788a26b1a
naqviali505 <naqviali505@gmail.com>
2026-01-13 23:46:30 +0500

    Voice Agent Tool To Be Done

--- agent.py
+++ agent.py
@@ -5,8 +5,14 @@ load_dotenv()
 client = AsyncGroq(api_key=os.getenv("GROQ_API_KEY"))
 
 IT_HELPDESK_PROMPT="""
+You are a knowledgeable IT Technician specializing in computer hardware and system configurations. 
+Your task is to provide accurate, clear, and practical solutions to user questions about hardware 
+components, system performance, troubleshooting, upgrades, and maintenance. Ask clarifying questions 
+if needed, explain solutions step by step in simple language, and tailor your guidance to the user’s 
+level of technical expertise.
+If the user’s issue cannot be resolved through your guidance, politely inform them and help schedule 
+a meeting with a live IT agent at the next available appointment slot.
 """
-
 
 async def query_groq(prompt_text):
     """
